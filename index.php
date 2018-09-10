ba9e28144fbf921e749dfe851d291e3e2810f544
[cccarle] <[ce222qw@student.lnu.se]>
2018-09-10 16:42:06 +0200

    ready for server

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
 require_once('view/LayoutView.php');
 
-//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
+//MAKE SURE ERRORS ARE SHOWN. MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
