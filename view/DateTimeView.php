947bf0363404edc1841e3ddb83fc04ef10a36767
[cccarle] <[ce222qw@student.lnu.se]>
2018-09-10 17:33:44 +0200

    servertime check

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -3,10 +3,26 @@
 class DateTimeView {
 
 
+	/* 
+	Returns a string with time from server.
+	*/
+	
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		date_default_timezone_set('Europe/Stockholm');
+		$info = getdate();
+		$weekday = $info['weekday'];
+		$date = $info['mday'];
+		$month = $info['month'];
+		$year = $info['year'];
+		$hour = $info['hours'];
+		$min = $info['minutes'];
+		$sec = $info['seconds'];
+
+		$timeString =  "$weekday, the {$date}th of $month $year, The time is  $hour:$min:$sec";
 
 		return '<p>' . $timeString . '</p>';
 	}
 }
+
+
