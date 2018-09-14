bb5c9e64aeaf4943d7fc26b9f940a9d003a679cb
[cccarle] <[ce222qw@student.lnu.se]>
2018-09-15 00:11:21 +0200

    test

--- config/config.php
+++ config/config.php
@@ -12,7 +12,7 @@ define('DB_NAME', 'Auth_1dv610');
 define('APPROOT', dirname(dirname(__FILE__)));
 
 // URL Root
-define('URLROOT', 'http://192.168.64.2/1dv610_assignment_2-master/');
+define('URLROOT', 'http://159.65.83.224/');
 
 // Site Name
 define('SITENAME', 'Auth_1dv610');
